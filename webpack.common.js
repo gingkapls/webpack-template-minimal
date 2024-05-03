740872637294ed495a2ebb854c65ac3cadeaa5ed
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-03 12:52:49 +0530

    webpack: add html-loader

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,10 @@ module.exports = {
         },
       },
       {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
