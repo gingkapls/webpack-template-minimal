3e6ffcc66b5947edd1104cd04d2faab989dfbb40
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-03 12:37:34 +0530

    webpack: add babel and babel-loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
+    index: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Hello World",
-      template: "./src/index.html",
+      title: 'Hello World',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            cacheDirectory: true,
+            cacheCompression: true,
+          },
+        },
+      },
+      {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
